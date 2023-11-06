d97393134b17f25b4caf2a0308ace0a904516431
Riseik <emaillard@gaming.tech>
2023-11-06 17:11:01 +0100

    translation

--- Transform.h
+++ Transform.h
@@ -16,19 +16,15 @@ private :
 
 
 	void Identity();
-	void FromMatrix(XMMATRIX* pMat);
 	void UpdateRotationFromVectors();
 	void UpdateRotationFromQuaternion();
-	void UpdateRotationFromMatrix();
 	void UpdateMatrix();
 	void Rotate(float yaw, float pitch, float roll);
 	void RotateYaw(float angle);
 	void RotatePitch(float angle);
 	void RotateRoll(float angle);
-	void RotateWorld(XMMATRIX* pMatrix);
-	void RotateWorldX(float angle);
-	void RotateWorldY(float angle);
-	void RotateWorldZ(float angle);
+
+	void Translate(float x, float y, float z);
 
 	XMFLOAT4X4 MultiplyFloat4X4(XMFLOAT4X4 mat1, XMFLOAT4X4 mat2);
 };
